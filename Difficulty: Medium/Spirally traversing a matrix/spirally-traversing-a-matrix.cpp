f1089de21e28c5931770b2c80a56cad0cf1a9cc6
Shubham <shubham.sharma4390@gmail.com>
2024-12-20 09:03:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> spirallyTraverse(vector<vector<int>> &mat) {
+        int N = mat.size(), M = mat[0].size();
+        vector<int> res;
+        
+        int top = 0, bottom = N - 1, left = 0, right = M - 1;
+        
+        while(top <= bottom && left <= right) {
+            for(int i = left; i <= right; ++i) {
+                res.push_back(mat[top][i]);
+            }
+            top ++;
+            
+            for(int i = top; i <= bottom; ++i) {
+                res.push_back(mat[i][right]);
+            }
+            right --;
+            
+            if(top <= bottom) {
+                for(int i = right; i >= left; --i) {
+                    res.push_back(mat[bottom][i]);
+                }
+                bottom --;
+            }
+            
+            if(left <= right) {
+                for(int i = bottom; i >= top; --i) {
+                    res.push_back(mat[i][left]);
+                }
+                left ++;
+            }
+        }
+        
+        return res; 
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r);
+
+        for (int i = 0; i < r; i++) {
+            matrix[i].assign(c, 0);
+            for (int j = 0; j < c; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix);
+        for (int i = 0; i < result.size(); ++i)
+            cout << result[i] << " ";
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
