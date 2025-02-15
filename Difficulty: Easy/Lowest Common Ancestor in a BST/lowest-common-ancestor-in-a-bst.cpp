6f08d09829b40e972b2f52863c1d96d81f2e64e4
Shubham <shubham.sharma4390@gmail.com>
2025-02-15 09:22:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Lowest Common Ancestor in a BST/lowest-common-ancestor-in-a-bst.cpp
+++ Difficulty: Easy/Lowest Common Ancestor in a BST/lowest-common-ancestor-in-a-bst.cpp
@@ -40,12 +40,10 @@ class Node {
 class Solution {
   public:
     Node* LCA(Node* root, Node* n1, Node* n2) {
-        if(!root) return nullptr; 
-        if(root -> data > n1 -> data && root -> data > n2 -> data) {
-            return LCA(root -> left, n1, n2);
-        }
-        if(root -> data < n1 -> data && root -> data < n2 -> data) {
-            return LCA(root -> right, n1, n2);
+        while(root != nullptr) {
+            if(root -> data > n1 -> data && root -> data > n2 -> data) root = root -> left;
+            else if(root -> data < n1 -> data && root -> data < n2 -> data) root = root -> right;
+            else break;
         }
         return root; 
     }
