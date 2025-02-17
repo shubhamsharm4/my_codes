5bdf73d5bb46a1206ab90ee43bd3df082a7f0337
Shubham <shubham.sharma4390@gmail.com>
2025-02-17 07:33:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/k largest elements/k-largest-elements.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> kLargest(vector<int>& arr, int k) {
+        vector<int> res;
+        priority_queue<int, vector<int>> pq;
+        //I think it'll store teh greater element on top (descending order)
+        for(int i : arr) pq.push(i);
+        while(k > 0) {
+            res.push_back(pq.top());
+            pq.pop();
+            k--;
+        }
+        
+        return res; 
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        vector<int> ans = ob.kLargest(arr, k);
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
